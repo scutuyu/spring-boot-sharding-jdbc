cee78f1a15ba1c3cd10d9af4431c35e6f90064a1
tuyu <scutuyu@foxmail.com>
2019-04-24 16:13:11 +0800

    perf(ActionLog.java): 使用lombok类库

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # spring-boot-sharding-jdbc
 spring-boot sharding-jdbc mybatis 分表 物理表按日期与逻辑表匹配 
+
+## 技术栈
+
+- spring boot 1.4.3.RELEASE
+- lombok 1.16.12
+- mybatis-spring-boot-starter 1.2.0
+- sharding-jdbc 1.4.1
+- mapper-spring-boot-starter 1.0.0
--- src/main/java/com/tuyu/sharding/entity/ActionLog.java
+++ src/main/java/com/tuyu/sharding/entity/ActionLog.java
@@ -1,8 +1,11 @@
 package com.tuyu.sharding.entity;
 
+import lombok.Data;
+
 /**
  * Created by tuyu on 1/11/17.
  */
+@Data
 public class ActionLog {
 
     private Long id;
@@ -22,101 +25,5 @@ public class ActionLog {
     private String serv;
 
     private String app;
-
-
-
-
-    @Override
-
-    public String toString() {
-        return "ActionLog{" +
-                "id=" + id +
-                ", date='" + date + '\'' +
-                ", group_id=" + group_id +
-                ", serv='" + serv + '\'' +
-                ", app='" + app + '\'' +
-                ", up_flux=" + up_flux +
-                ", down_flux=" + down_flux +
-                ", groupId=" + groupId +
-                ", name='" + name + '\'' +
-                ", serv='" + serv + '\'' +
-                ", app='" + app + '\'' +
-                '}';
-    }
-
-
-
-    public void setGroupId(Integer groupId) {
-        this.groupId = groupId;
-    }
-
-    public void setServ(String serv) {
-        this.serv = serv;
-    }
-
-    public void setApp(String app) {
-        this.app = app;
-    }
-
-    public Integer getGroupId() {
-
-        return groupId;
-    }
-
-    public String getServ() {
-        return serv;
-    }
-
-    public String getApp() {
-        return app;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getDate() {
-        return date;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public void setDate(String date) {
-        this.date = date;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Integer getGroup_id() {
-        return group_id;
-    }
-
-    public void setGroup_id(Integer group_id) {
-        this.group_id = group_id;
-    }
-
-    public void setUp_flux(Long up_flux) {
-        this.up_flux = up_flux;
-    }
-
-    public void setDown_flux(Long down_flux) {
-        this.down_flux = down_flux;
-    }
-
-    public Long getUp_flux() {
-        return up_flux;
-    }
-
-    public Long getDown_flux() {
-        return down_flux;
-    }
 
 }
